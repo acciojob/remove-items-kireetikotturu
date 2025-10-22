21533d30635e1b9e748a111583a0d94ac6d51687
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-22 11:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+    let dropDown = document.getElementById("colorSelect")
+  let button = document.getElementsByTagName("input")[0];
+
+  button.addEventListener("click", ()=>{
+    dropDown.removeChild(dropDown[0])
+  })
